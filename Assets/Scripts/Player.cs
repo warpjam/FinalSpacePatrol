c8c7ee9f3a3a8ef0e32cf3a6737f622d7dc5c0d6
warpjam <warpjam@gmail.com>
2023-01-20 15:55:36 -0500

    Refactored Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,14 +14,25 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _playerSpeed * Time.deltaTime);
 
-        if (transform.position.y >= 5.7f)
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -5.0f, 5.0f), 0);
+
+        if (transform.position.x > 11f)
         {
-            
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11f)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
         }
     }
 }
