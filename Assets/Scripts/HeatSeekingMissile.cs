0d29787d96376716294b78e80bd8f7fa0bb1fd20
warpjam <warpjam@gmail.com>
2023-05-04 11:39:00 -0400

    Missile will damage the enemy

--- Assets/Scripts/HeatSeekingMissile.cs
+++ Assets/Scripts/HeatSeekingMissile.cs
@@ -26,10 +26,34 @@ public class HeatSeekingMissile : MonoBehaviour
             Vector3 direction = _player.transform.position - transform.position;
             direction.Normalize();
             transform.Translate(direction * _speed * Time.deltaTime, Space.World);
+            
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
         }
         else
         {
             transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Damage player
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+            Destroy(gameObject);
+        }
+        else if (other.CompareTag("Laser"))
+        {
+            // Destroy laser and self
+            Destroy(other.gameObject);
+            Destroy(gameObject);
         }
     }
 }
